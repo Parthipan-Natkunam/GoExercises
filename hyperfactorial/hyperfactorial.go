ecb1c49799f848e0fec3dc799c1214d812311cd5
Parthipan-Natkunam <pnatkunam@babbel.com>
2025-09-29 23:53:41 +0200

    feat: add solution for hyperfactorial

--- hyperfactorial/hyperfactorial.go
+++ hyperfactorial/hyperfactorial.go
@@ -1,7 +1,16 @@
 package hyperfactorial
 
-import "fmt"
+import "math/big"
 
-func main() {
-	fmt.Println("Hello from hyperfactorial!")
+const NEGATIVE_INPUT_ERROR = "n must be a non-negative integer"
+
+func Hyperfactorial(n int) *big.Int {
+	if n < 0 {
+		panic(NEGATIVE_INPUT_ERROR)
+	}
+	result := big.NewInt(1)
+	for i := 1; i <= n; i++ {
+		result.Mul(result, big.NewInt(int64(i)).Exp(big.NewInt(int64(i)), big.NewInt(int64(i)), nil))
+	}
+	return result
 }
