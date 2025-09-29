e1793c7e159f1b1ad2419aed36f1e96cd2d011a0
Parthipan-Natkunam <pnatkunam@babbel.com>
2025-09-30 00:27:57 +0200

    feat: implement method to calculate hyperfactorial using the asymptotic approximation method

--- hyperfactorial/hyperfactorial.go
+++ hyperfactorial/hyperfactorial.go
@@ -1,6 +1,9 @@
 package hyperfactorial
 
-import "math/big"
+import (
+	"math"
+	"math/big"
+)
 
 func Hyperfactorial(n int) *big.Int {
 	if n < 0 {
@@ -11,4 +14,17 @@ func Hyperfactorial(n int) *big.Int {
 		result.Mul(result, big.NewInt(int64(i)).Exp(big.NewInt(int64(i)), big.NewInt(int64(i)), nil))
 	}
 	return result
+}
+
+func HyperfactorialAsymptotic(n int) float64 {
+	if n < 0 {
+		panic("n must be a non-negative integer")
+	}
+	if n == 0 {
+		return 1
+	}
+	const glaisherKinkelinConstant = 1.28242712910062261468
+	firstExponent := float64(n*n + n) / 2 + 1.0/12.0
+	secondExponent := float64(n*n)/4
+	return math.Round(glaisherKinkelinConstant * math.Pow(float64(n), firstExponent) * math.Exp(-secondExponent))
 }
